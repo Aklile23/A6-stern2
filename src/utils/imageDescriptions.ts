e8a964464044035e008a29d9e052d4ffd0ce9f00
Aklile <akliletilahun@gmail.com>
2024-11-15 01:50:37 +0300

    description template DONE

--- src/utils/imageDescriptions.ts
+++ src/utils/imageDescriptions.ts
@@ -1,8 +1,12 @@
 const imageDescriptions: Record<string, string> = {
-    "panoramas/20241007/room02.jpg": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    "panoramas/20241009/room02.jpg": "Room 03 Room 03 Room 03 Room 03 Room 03 Room 03 Room 03 Room 03 Room 03 Room 03 Room 03 Room 03",
-    "room04.jpg": " Room 04 Room 04 Room 04 Room 04 Room 04 Room 04 Room 04 Room 04",
-    "panoramas/Room2/20241007/room02.jpg": "Room 02 Room 02 Room 02 Room 02 Room 02 Room 02 Room 02 Room 02 Room 02 Room 02 Room 02 Room 02 Room 02 Room 02 Room 02 Room 02 Room 02 Room 02 Room 02 Room 02 Room 02 Room 02 Room 02 Room 02 Room 02 Room 02 Room 02 Room 02 Room 02 Room 02 Room 02 Room 02",
+    "panoramas/20241007/room02.jpg": "Room 2, room02.jpg, 2024-10-07, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    "panoramas/20241007/room03.jpg": "Room 3, room03.jpg, 2024-10-07, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    "panoramas/20241009/room02.jpg": "Room 2, room02.jpg, 2024-10-09, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    "panoramas/20241009/room03.jpg": "Room 2, room03.jpg, 2024-10-09, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    "panoramas/20241009/room04.jpg": "Room 2, room04.jpg, 2024-10-09, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    "panoramas/20241009/room05.jpg": "Room 2, room05.jpg, 2024-10-09, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    "panoramas/20241009/room06.jpg": "Room 2, room06.jpg, 2024-10-09, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+
     // Add more descriptions here
 };
 
